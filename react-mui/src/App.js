4bcf02cad0d6bb84642a6775b8b6e0c7169d34b6
Ohjju <ojh011223@naver.com>
2023-09-10 16:33:43 +0900

    MUI test

---
+++ react-mui/src/App.js
@@ -0,0 +1,28 @@
+import Card from "./Card.jsx";
+import Paper from "./Paper.jsx";
+import Avatar from "./Avatar.jsx";
+import Radio from "./Radio.jsx";
+import Modal from "./Modal.jsx";
+
+function App() {
+  return (
+    <>
+      <Card />
+      <br></br>
+      <br></br>
+      <Modal />
+      <br></br>
+      <br></br>
+      <Paper />
+      <br></br>
+      <br></br>
+      <Avatar></Avatar>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Radio />
+    </>
+  );
+}
+
+export default App;
