a7bd2aa0bf6537ca1acd4ae83ffa976b58003def
Ohjju <ojh011223@naver.com>
2023-09-28 21:33:20 +0900

    teamproject2 수정

--- teamProject2/Todo.jsx
+++ teamProject2/Todo.jsx
@@ -1,6 +1,8 @@
 import "../styles/global.scss";
 import "../styles/todo.scss";
 import TodoBox from "../components/TodoBox";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 // 할 일
 export default function Todo() {
@@ -15,9 +17,39 @@ export default function Todo() {
         <div className="right">
           <div className="todoGroup">
             <div className="todoGroupingBox">
-              <div className="todo"></div>
-              <div className="doing"></div>
-              <div className="done"></div>
+              <div className="todo">
+                <div style={{ display: "flex" }}>
+                  <div className="state"></div>
+                  <div className="title">해야할 일</div>
+                  <div className="count">5</div>
+                </div>
+                <FontAwesomeIcon
+                  icon={faPlus}
+                  style={{ color: "d9d9d9", fontSize: "2.5rem" }}
+                />
+              </div>
+
+              <div className="doing">
+                <div style={{ display: "flex" }}>
+                  <div className="state"></div>
+                  <div className="title">진행 중</div>
+                  <div className="count">5</div>
+                </div>
+                <FontAwesomeIcon
+                  icon={faPlus}
+                  style={{ color: "d9d9d9", fontSize: "2.5rem" }}
+                />
+              </div>
+              <div className="done">
+                <div style={{ display: "flex" }}>
+                  <div className="state"></div>
+                  <div className="title">완료</div>
+                </div>
+                <FontAwesomeIcon
+                  icon={faPlus}
+                  style={{ color: "d9d9d9", fontSize: "2.5rem" }}
+                />
+              </div>
             </div>
             <div className="todoGroupBox">
               <div className="todo">
@@ -25,16 +57,22 @@ export default function Todo() {
                   title="디자인 틀 완성하기"
                   date="09.28 ~ 10.20"
                   content="내용내요내용내용내요내요"
+                  boxcolor="--select-pink"
+                  statecolor="--status-green"
                 />
                 <TodoBox
                   title="코드 작성하기"
                   date="09.28 ~ 10.20"
                   content="content"
+                  boxcolor="--select-gray"
+                  statecolor="--status-green"
                 />
                 <TodoBox
                   title="코드 작성하기"
                   date="09.28 ~ 11.30"
                   content="content"
+                  boxcolor="--select-green"
+                  statecolor="--status-green"
                 />
               </div>
               <div className="doing">
@@ -42,11 +80,15 @@ export default function Todo() {
                   title="코드 작성하기"
                   date="09.28 ~ 11.30"
                   content="content"
+                  boxcolor="--select-orange"
+                  statecolor="--status-yellow"
                 />
                 <TodoBox
                   title="코드 작성하기"
                   date="09.28 ~ 11.30"
                   content="content"
+                  boxcolor="--select-purple"
+                  statecolor="--status-yellow"
                 />
               </div>
               <div className="done">
@@ -54,6 +96,8 @@ export default function Todo() {
                   title="코드 작성하기"
                   date="09.28 ~ 11.30"
                   content="content"
+                  boxcolor="--select-brown"
+                  statecolor="--status-gray"
                 />
               </div>
             </div>
--- teamProject2/TodoBox.jsx
+++ teamProject2/TodoBox.jsx
@@ -1,11 +1,35 @@
 import "../styles/todoBox.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 
 export default function TodoBox(props) {
-  const { title, date, content } = props;
+  const { title, date, content, statecolor, boxcolor } = props;
   return (
     <>
       <div className="todoBox">
+        <div
+          className="todoColor"
+          style={{ backgroundColor: `var(${boxcolor})` }}
+        ></div>
         <div className="todoContent">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "100%",
+              paddingRight: "1.6rem",
+            }}
+          >
+            <div
+              className="state"
+              style={{ backgroundColor: `var(${statecolor})` }}
+            ></div>
+            <FontAwesomeIcon
+              icon={faEllipsisVertical}
+              style={{ color: "d9d9d9", fontSize: "2.5rem" }}
+            />
+          </div>
+
           <div className="todoTitle">{title}</div>
           <div className="todoDate">{date}</div>
           <div className="todoMemo">{content}</div>
